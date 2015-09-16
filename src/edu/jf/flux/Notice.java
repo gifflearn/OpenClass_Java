30bdabe654dfc100f70951ec6ef5cc1116ec5908
JFL <jean-francois.lahellec@atmel.com>
2015-09-16 17:49:54 +0200

    Mercredi soir

---
+++ src/edu/jf/flux/Notice.java
@@ -0,0 +1,13 @@
+package edu.jf.flux;
+public class Notice {
+	  private String langue ;
+	  public Notice(){
+	    this.langue = "Français";
+	  }
+	  public Notice(String lang){
+	    this.langue = lang;
+	  }
+	  public String toString() {
+	    return "\t Langue de la notice : " + this.langue + "\n";
+	  }
+	}
